1857d23237fdd57e29fedd3143ce286467d3b042
c60603 <c60603@@gmail.com>
2021-10-18 19:44:34 +0800

    Initial Commit

---
+++ PathAndShape/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PathAndShape
+//
+//  Created by User08 on 2021/10/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PathAndShape/PathAndShapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PathAndShapeApp.swift
+//  PathAndShape
+//
+//  Created by User08 on 2021/10/18.
+//
+
+import SwiftUI
+
+@main
+struct PathAndShapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
